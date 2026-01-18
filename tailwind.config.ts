c2ec4dbc0c80bbebac82fcdd820206950b1cddc4
Dorian <dorian.thome@proton.me>
2026-01-18 10:40:25 +0100

    rerun

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // ⚠️ THIS IS THE FIX: Explicitly pointing to root folders
+  // ⚠️ THIS IS THE FIX: Explicitly pointing to root folderss
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
